9f898fe500b3cc469482014003448809ac098a5d
KlakCodes <harrisonjclark@hotmail.com>
2024-11-15 15:20:33 +0000

    Added comments to the code

--- scripts.js
+++ scripts.js
@@ -1,13 +1,10 @@
+// The Gameboard represents the state of the board
 function Gameboard() {
   const rows = 3;
   const columns = 3;
-
-  // const board = Array.from(Array(rows), () => new Array(columns));
-  // board[0][0] = 'test';
-  // console.info(board);
-
   const board = [];
 
+  // Create a 2d array that will represent the state of the game board
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
@@ -15,8 +12,11 @@ function Gameboard() {
     }
   }
 
+  // Get the game board in it's current state
   const getBoard = () => board;
 
+  // Place a token on the board
+  // Check if there is already a token in selected cell
   const placeToken = (row, column, player) => {
     if (board[row][column].getValue() !== 0) {
       console.log(`Cell [${row}][${column}] is already occupied!`);
@@ -26,21 +26,26 @@ function Gameboard() {
     board[row][column].addToken(player);
   };
 
+  // Print board to the console
   const printBoard = () => {
     const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
     console.log(boardWithCellValues);
   };
 
+  // Provide an interface for the rest of the application to interact with the board
   return { getBoard, placeToken, printBoard };
 }
 
+// A cell represents each square of the board
 function Cell() {
   let value = 0;
 
+  // Accept a player's token to change the value of the cell
   const addToken = (player) => {
     value = player;
   };
 
+  // Retrieve the current value of this cell through closure
   const getValue = () => value;
 
   return {
@@ -49,6 +54,8 @@ function Cell() {
   };
 }
 
+/* The GameController will be responsible for controlling the  flow and state of the game's
+turns, as well as whether anybody has won the game */
 function GameController(
   playerOneName = "Player One",
   playerTwoName = "Player Two"
@@ -79,6 +86,8 @@ function GameController(
   };
 
   const playRound = (row, column) => {
+    // Drop a token for the current player
+    // If an invalid cell is chosen, ask the user to select a different cell
     if (board.getBoard()[row][column].getValue() !== 0) {
       console.log(`Cell [${row}][${column}] is already occupied! ${getActivePlayer().name}, please choose a different cell.`);
       return; // Do not switch the turn if the cell is occupied
@@ -89,10 +98,12 @@ function GameController(
     );
     board.placeToken(row, column, getActivePlayer().token);
 
+    // Switch player turn
     switchPlayerTurn();
     printNewRound();
   };
 
+  // Initial play game message
   printNewRound();
 
   return {
